11c4df63083188d8905dae67a6bd0224359c0f2e
iwork <iwork30rus@gmail.com>
2021-08-19 18:24:34 +0400

    1. The path to feature file was changed

--- src/test/java/com/epam/tc/hw5/cucumber/RunAcceptanceTest.java
+++ src/test/java/com/epam/tc/hw5/cucumber/RunAcceptanceTest.java
@@ -4,9 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
-        features = {"src/test/resources/com.epam.tc.hw5.cucumber/exercis1/indexPageCheckElementsExist.feature",
-                "src/test/resources/com.epam.tc.hw5.cucumber/exercise2/tablePageCheckUserInformation.feature",
-                "src/test/resources/com.epam.tc.hw5.cucumber/exercise3/tablePageCheckStatusWasChanged.feature"})
+        features = {"src/test/resources/com.epam.tc.hw5.cucumber/"})
 public class RunAcceptanceTest extends AbstractTestNGCucumberTests {
 
 }
