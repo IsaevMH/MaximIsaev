02d1ad5a4628680776017be545d1bc407719c591
iwork <iwork30rus@gmail.com>
2021-08-19 18:30:04 +0400

    1. The path to feature file in resources was changed

--- src/test/java/com/epam/tc/hw5/cucumber/RunAcceptanceTest.java
+++ src/test/java/com/epam/tc/hw5/cucumber/RunAcceptanceTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
-        features = {"src/test/resources/com.epam.tc.hw5.cucumber/"})
+        features = {"src/test/resources/hw5/"})
 public class RunAcceptanceTest extends AbstractTestNGCucumberTests {
 
 }
