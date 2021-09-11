299cbc5f2ecf5cff29f5720593e36d7067c16f29
iwork <iwork30rus@gmail.com>
2021-09-11 14:51:28 +0400

    1. Issues were fixed

--- src/test/java/com/epam/tc/hw7/section/LoginForm.java
+++ src/test/java/com/epam/tc/hw7/section/LoginForm.java
@@ -1,16 +1,10 @@
 package com.epam.tc.hw7.section;
 
 import com.epam.jdi.light.elements.composite.Form;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
-import com.epam.jdi.light.ui.html.elements.common.Image;
-import com.epam.jdi.light.ui.html.elements.common.Text;
 import com.epam.jdi.light.ui.html.elements.common.TextField;
 import com.epam.tc.hw7.entity.LoginFormData;
-import com.epam.tc.hw7.service.DataReader;
-
-import static com.epam.tc.hw7.page.IndexPage.userIcon;
 
 public class LoginForm extends Form<LoginFormData> {
 
@@ -23,7 +17,7 @@ public class LoginForm extends Form<LoginFormData> {
 
     @Override
     public void fill(LoginFormData loginFormData) {
-        userIcon.click();
+        //userIcon.click();
         login.setValue(loginFormData.getLogin());
         password.setValue(loginFormData.getPassword());
         signInButton.click();
